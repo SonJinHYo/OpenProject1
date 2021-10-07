f0ac21e32c45a3a39fd8be68b8af51b044c93df9
SonJinHYo <wlsgy95@naver.com>
2021-10-07 14:23:46 +0900

    git test

---
+++ src/edu/Main.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package edu;
+
+
+/**
+ * @author 손진효
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//변수  선언
+		int firstNo,secondNo;
+		int sum;
+		
+		//변수 입력
+		firstNo = 11;
+		
+		secondNo = 23;
+		
+		//합계내기
+		sum = firstNo + secondNo;
+		
+		//합계 출력하기
+		System.out.print("입력한 두 수의 합계 : "+sum);
+		
+
+	}
+
+}
+ 
